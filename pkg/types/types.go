b797dd28b36ada19eb59de1196c50a1223b810b7
s_zer0 <shoha2105@gmail.com>
2020-10-03 23:00:09 +0500

    Изменён тип Payment, добавлено поле AccountID

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -18,10 +18,11 @@ const (
 
 // Payment представляет информацию о платеже.
 type Payment struct {
-	ID		 string
-	Amount	 Money
-	Category PaymentCategory
-	Status	 PaymentStatus
+	ID		  string
+	AccountID int64
+	Amount	  Money
+	Category  PaymentCategory
+	Status	  PaymentStatus
 }
 
 type Phone string
