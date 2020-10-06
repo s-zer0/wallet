650b658b842cf843801a586036abc75f68b45657
s_zer0 <shoha2105@gmail.com>
2020-10-06 10:28:42 +0500

    deleted comments

--- pkg/wallet/example_test.go
+++ pkg/wallet/example_test.go
@@ -2,7 +2,6 @@ package wallet
 
 import (
 	"reflect"
-	"fmt"
 	"testing"
 	"github.com/s-zer0/wallet/pkg/types"
 )
@@ -94,55 +93,6 @@ func TestService_FindPaymentByID_success(t *testing.T) {
 		t.Errorf("FindPaymentByID(): wrong payment returned = %v", err)
 		return
 	}
-}
-// func TestService_FindPaymentByID_Success(t *testing.T) {
-// 	svc := &Service{}
-// 	svc.RegisterAccount("+9920000001")
-
-// 	payment, err := svc.Pay(2, types.Money(110), "auto")
-
-// 	if err != nil {
-// 		switch err {
-// 			case ErrAmountMustBePositive:
-// 				fmt.Println("Сумма должна быть положительной")
-// 			case ErrAccountNotFound:
-// 				fmt.Println("Аккаунт пользователя не найден")
-// 			case ErrNotEnoughBalance:
-// 				fmt.Println("Недостаточно средств на балансе")
-// 			}
-// 			return 
-// 	}
-
-// 	result, err := svc.FindPaymentByID(payment.ID)
-// 	if err != nil{
-// 		fmt.Println("Платёж не найден")
-// 	}
-
-// 	if !reflect.DeepEqual(payment, result) {
-// 		t.Errorf("invalid result, expected: %v, actual: %v", payment, result)
-// 	}
-// }
-
-type testService struct {
-	*Service
-}
-
-func newTestService() *testService {
-	return &testService{Service: &Service{}}
-}
-
-func (s *testService) addAccountWithBalance(phone types.Phone, balance types.Money) (*types.Account, error) {
-	account, err := s.RegisterAccount(phone)
-	if err != nil {
-		return nil, fmt.Errorf("can't register account, error=%v", err)
-	}
-
-	err = s.Deposit(account.ID, balance)
-	if err != nil {
-		return nil, fmt.Errorf("can't deposit account, error = %v", err)
-	}
-
-	return account, nil
 }
 
 func TestService_Repeat_Success(t *testing.T) {
